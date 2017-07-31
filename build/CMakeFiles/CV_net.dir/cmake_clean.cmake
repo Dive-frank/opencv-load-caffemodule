9dd64455f1c7b5f8e0e9e93c49be6e5a1bc73cf4
KAM <fangcm0101@foxmail.com>
2017-07-31 11:16:08 +0800

    add all files

---
+++ build/CMakeFiles/CV_net.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/CV_net.dir/main.cpp.o"
+  "../bin/CV_net.pdb"
+  "../bin/CV_net"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/CV_net.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
